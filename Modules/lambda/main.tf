4011032cca91d70f0df93492cd82cb7652546e3c
stestyes2026 <stestyes2026@outlook.com>
2026-04-08 22:50:45 +0530

    Remove output for IAM role ARN in main.tf
    Remove output block for IAM role ARN.

--- Modules/lambda/main.tf
+++ Modules/lambda/main.tf
@@ -18,6 +18,3 @@ resource "aws_iam_policy_attachment" "name" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-output "role" {
-  value = aws_iam_role.lambdarole.arn
-}
